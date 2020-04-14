eae489965fa1bbf6ddd0d5b4ec497ae50f76e0e7
sebssch <ssebsch@googlemail.com>
2020-04-14 14:56:07 +0200

    Add new translations, Add some schema.org tags in the template, Add some css-classes in the template

--- src/Resources/contao/languages/de/tl_user.php
+++ src/Resources/contao/languages/de/tl_user.php
@@ -7,10 +7,10 @@ declare(strict_types=1);
  * (c) Werbeagentur Dreibein GmbH
  */
 
-$GLOBALS['TL_LANG']['tl_user']['author_legend'] = 'Autoreneinstellungen';
+$GLOBALS['TL_LANG']['tl_user']['author_legend'] = 'Autor-Einstellungen';
 $GLOBALS['TL_LANG']['tl_user']['authorPicture'] = ['Bild', 'Wählen Sie hier das Bild des Benutzers aus.'];
 $GLOBALS['TL_LANG']['tl_user']['authorDescription'] = ['Beschreibung', 'Geben Sie hier eine Beschreibung des Benutzers ein.'];
-$GLOBALS['TL_LANG']['tl_user']['authorLinks'] = ['Links', 'Fügen Sie hier Links zum Benutzer ein.'];
+$GLOBALS['TL_LANG']['tl_user']['authorLinks'] = ['Links', 'Fügen Sie hier Social-Links zum Benutzer ein.'];
 $GLOBALS['TL_LANG']['tl_user']['authorLinkName'] = ['Bezeichnung', ''];
 $GLOBALS['TL_LANG']['tl_user']['authorLink'] = ['Link', ''];
 $GLOBALS['TL_LANG']['tl_user']['authorPage'] = ['Seite', 'Wählen Sie hier die Seite des Benutzers aus.'];
